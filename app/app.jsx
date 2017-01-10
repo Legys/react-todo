aa104818a05cc33144a58f1b31fbb26b2ca8c18f
Alexandr Dubrovin <legys5@yandex.ru>
2017-01-10 18:57:30 +0200

    initial commit

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
+var TodoApp = require('TodoApp');
 
 // Load foundation
 $(document).foundation();
@@ -10,13 +10,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-    <Router history={hashHistory} >
-        <Route path="/" component={Main} >
-           {/* <Route path="countdown" component={Countdown} />
-            <IndexRoute component={Timer}/>*/}
-
-        </Route>
-
-    </Router>,
+    <TodoApp/>,
     document.getElementById('app')
 );
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+var TodoApp = React.createClass({
+    render() {
+        return (
+            <div>
+                This is TodoApp.jsx
+            </div>
+        )
+    }
+});
+module.exports = TodoApp;
