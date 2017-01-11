28f09b300114fe93e140eb2c64c0e8173d462d69
Alexandr Dubrovin <legys5@yandex.ru>
2017-01-11 14:03:26 +0200

    added tests

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,35 @@
 var React = require('react');
+var TodoList = require('TodoList');
+
 var TodoApp = React.createClass({
+    getInitialState() {
+        return {
+            todos: [
+                {
+                  id: 1,
+                  text: 'Выгулять собаку'
+                },
+                {
+                    id: 2,
+                    text: 'Почистить двор'
+                },
+                {
+                    id: 3,
+                    text: 'Позаниматься английским'
+                },
+                {
+                    id: 4,
+                    text: 'Написать приложение'
+                }
+            ]
+        }
+    },
     render() {
+        var {todos} = this.state;
+
         return (
             <div>
-                This is TodoApp.jsx
+                <TodoList todos={todos}/>
             </div>
         )
     }
